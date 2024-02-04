fd5aa6c73aa31b87ab31feb9c1a86e81be5dd5c6
FadyShakerShawky <79304352+FadyShakerShawky@users.noreply.github.com>
2024-02-04 20:56:14 +0200

    #auth2

--- config/config.php
+++ config/config.php
@@ -4,7 +4,8 @@ return [
     'api_key' => env('ZOOM_CLIENT_KEY'),
     'api_secret' => env('ZOOM_CLIENT_SECRET'),
     'base_url' => 'https://api.zoom.us/v2/',
+    'account_id' => env('ZOOM_ACCOUNT_ID'),
+    'authentication_method' => 'oauth2', // jwt and OAuth2 compatible
     'token_life' => 60 * 60 * 24 * 7, // In seconds, default 1 week
-    'authentication_method' => 'jwt', // Only jwt compatible at present but will add OAuth2
     'max_api_calls_per_request' => '5' // how many times can we hit the api to return results for an all() request
 ];
--- src/Support/Entry.php
+++ src/Support/Entry.php
@@ -17,6 +17,7 @@ class Entry extends ApiEntry
     protected $apiKey = null;
 
     protected $apiSecret = null;
+    protected $account_id = null;
 
     protected $tokenLife = null;
 
@@ -44,10 +45,11 @@ class Entry extends ApiEntry
      * @param $maxQueries
      * @param $baseUrl
      */
-    public function __construct($apiKey = null, $apiSecret = null, $tokenLife = null, $maxQueries = null, $baseUrl = null)
+    public function __construct($apiKey = null, $apiSecret = null, $tokenLife = null, $maxQueries = null, $baseUrl = null, $account_id = null)
     {
         $this->apiKey = $apiKey ? $apiKey : config('zoom.api_key');
         $this->apiSecret = $apiSecret ? $apiSecret : config('zoom.api_secret');
+        $this->account_id = $account_id ? $account_id : config('zoom.account_id');
         $this->tokenLife = $tokenLife ? $tokenLife : config('zoom.token_life');
         $this->maxQueries = $maxQueries ? $maxQueries : (config('zoom.max_api_calls_per_request') ? config('zoom.max_api_calls_per_request') : $this->maxQueries);
         $this->baseUrl = $baseUrl ? $baseUrl : config('zoom.base_url');
@@ -55,9 +57,17 @@ class Entry extends ApiEntry
 
     public function newRequest()
     {
-        if (config('zoom.authentication_method') == 'jwt') {
-            return $this->jwtRequest();
-        } elseif (config('zoom.authentication_method') == 'oauth2') {
+        $zoom_supported_authentication_methods = [
+            'jwt' => function () {
+                return $this->jwtRequest();
+            },
+            'oauth2' => function () {
+                return $this->oauth2Request();
+            }
+        ];
+
+        if (array_key_exists(config('zoom.authentication_method'), $zoom_supported_authentication_methods)) {
+            return $zoom_supported_authentication_methods[config('zoom.authentication_method')]();
         }
     }
 
@@ -70,5 +80,23 @@ class Entry extends ApiEntry
 
     public function oauth2Request()
     {
+        if (session()->has('zoom_oauth_token') || session()->get('zoom_oauth_token_expire_in') > time()) {
+            return  Client::baseUrl($this->baseUrl)->withToken(session()->get('zoom_oauth_token')['access_token']);
+        } else {
+            $zoom_token_res = $this->generateAccessToken();
+            if ($zoom_token_res->status() === 200) {
+                session(['zoom_oauth_token' => $zoom_token_res->json()]);
+                session(['zoom_oauth_token_expire_in' => strtotime(time() + $zoom_token_res->json()['expires_in'])]);
+                return  Client::baseUrl($this->baseUrl)->withToken(session()->get('zoom_oauth_token')['access_token']);
+            }
+        }
+    }
+    public function generateAccessToken($grant_type = "account_credentials")
+    {
+        return  Client::baseUrl("https://zoom.us/oauth/token")
+            ->withBasicAuth($this->apiKey, $this->apiSecret)->asForm()->post('',  [
+                "grant_type" => $grant_type,
+                "account_id" => $this->account_id
+            ]);
     }
 }
